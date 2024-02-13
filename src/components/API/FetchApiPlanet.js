2178932d326ecbf37b499d8eea8696f0f3ab4bbf
Antonin Porcher <antoninporcher21@gmail.com>
2024-02-13 16:48:57 +0100

    création de la page destination

--- src/components/API/FetchApiPlanet.js
+++ src/components/API/FetchApiPlanet.js
@@ -1,30 +1,77 @@
 import { useEffect, useState } from "react"
 import React from 'react'
+import "./styles/FetchApiPlanet.css"
 
 function FetchApiPlanet() {
 
     const [data, setData] = useState([]);
-
+    const [selectedPlanet, setSelectedPlanet] = useState('statePlanet');
+    const dataImage = selectedPlanet.image_path;
+    console.log(useState([]));
     const apiGetPlanet = () => {
         fetch('http://spacetourism.test/api/planet')
           .then(response => response.json())
           .then((json) => {
             console.log(json);
             setData(json);
+            if (json.length > 0) {
+                setSelectedPlanet(json[0]); // Sélectionner la première planète par défaut
+              }
           });
     };
 
     useEffect(() => {
         apiGetPlanet();
     }, []);
+
+    const handlePlanetClick = (planet) => {
+        setSelectedPlanet(planet);
+      };
+
     return (
-            <div>
-                <ul>
-                    {data.map((item) => (
-                        <li key={item.id}>{item.title}</li>
-                    ))}
-                </ul>
+        <div className="planet-container">
+            <div className="gauchePlanet">
+                <div className="titreDestination">
+                    <span className="numeroDestination">01</span>CHOOSE YOUR DESTINATION
+                </div>
+                <div className="imagePlanet">
+                    <img src={`data:image/jpeg;base64,${dataImage}`} alt={selectedPlanet.title} />
+                </div>
             </div>
+            <div className="droitePlanet">
+                <div className="planet-list">
+                <ul>
+  {data.map((planet) => (
+    <li
+      key={planet.id}
+      onClick={() => handlePlanetClick(planet)}
+      className={planet === selectedPlanet ? 'selectedNav' : ''}
+    >
+      {planet.title}
+    </li>
+  ))}
+</ul>
+                </div>
+                <div className="planet-details">
+                {selectedPlanet && (
+                  <div>
+                    <p className="titrePlanet">{selectedPlanet.title}</p>
+                    <p className="detailPlanet">{selectedPlanet.detail}</p>
+                    <div className="statsPlanet">
+                        <div className="titreInfoPlanet">
+                        DISTANCE
+                        <div className="infoPlanet">{selectedPlanet.distance}</div>
+                        </div>
+                        <div className="titreInfoPlanet">
+                        DURATION
+                        <div className="infoPlanet">{selectedPlanet.duree}</div>
+                        </div>
+                    </div>
+                  </div>
+                )}
+                </div>
+            </div>
+        </div>
     )
 }
 
