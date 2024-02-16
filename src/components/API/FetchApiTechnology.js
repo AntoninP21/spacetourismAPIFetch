edb3117e7fa7eff6a0a6f19e868ed1e54ae142f5
Antonin Porcher <antoninporcher21@gmail.com>
2024-02-16 10:10:34 +0100

    Finalsation de la page technologie

--- src/components/API/FetchApiTechnology.js
+++ src/components/API/FetchApiTechnology.js
@@ -1,38 +1,68 @@
+import { useEffect, useState } from "react"
 import React from 'react'
+import BarNav from "./BarNav";
+import "./styles/FetchApiTechnology.css"
 
 function FetchApiTechnology() {
+const [data, setData] = useState([]);
+const [selectedTechnology, setSelectedTechnology] = useState('stateTechnology');
+const dataImage = selectedTechnology.image_path;
+console.log(useState([]));
+let technoNumber = 0
+
+const apiGetTechnology = () => {
+    fetch('http://spacetourism.test/api/technologie')
+      .then(response => response.json())
+      .then((json) => {
+        console.log(json);
+        setData(json);
+        if (json.length > 0) {
+            setSelectedTechnology(json[0]); 
+          }
+      });
+};
+
+useEffect(() => {
+    apiGetTechnology();
+}, []);
+
+const handleTechnologyClick = (technology) => {
+    setSelectedTechnology(technology);
+  };
+
   return (
     <div className='bodyTechnology'>
-         <div className=main_technologie>
-      <div className=titre_lancement>
-         <span className=numero>03</span>@lang('LANCEMENT SPATIAL') 101
+      <BarNav/>
+         <div className="main_technologie">
+      <div className="titre_lancement">
+         <span className="numero">03</span>LANCEMENT SPATIAL 101
       </div>
-      <div className=ensemble_technologie>
-         <div className=boutons_technologie>
-            <a href='{{route("technologie",["locale"=>App::getLocale()])}}' className=bouton_techno><div className=numero_boutons>1</div></a>
-            <a href='{{route("technologiePage2",["locale"=>App::getLocale()])}}' className=bouton_techno><div className=numero_boutons>2</div></a>
-            <a href='{{route("technologiePage3",["locale"=>App::getLocale()])}}' className=bouton_techno><div className=numero_boutons>3</div></a>
-            @foreach(Technologie::all() as $technologieItem)
-            @if(($technologieItem->title)==($technologie->title))
-            <a href='{{route("technologieAutre",["locale"=>App::getLocale(),"technologieName"=>$technologieItem->title])}}' className=bouton_techno_selected><div className=numero_boutons>{{ $loop->index +4}}</div></a>
-            @else
-            <a href='{{route("technologieAutre",["locale"=>App::getLocale(),"technologieName"=>$technologieItem->title])}}' className=bouton_techno><div className=numero_boutons>{{ $loop->index +4}}</div></a>
-            @endif
-            @endforeach
+      <div className="ensemble_technologie">
+         <div className="boutons_technologie">
+            <ul>
+               {data.map((technology) => (
+                 <li
+                   key={technology.id}
+                   onClick={() => handleTechnologyClick(technology)}
+                   >
+                     <div className={technology ===selectedTechnology ? 'bouton_techno_selected' :'bouton_techno'}><div className="numero_boutons">{technoNumber=technoNumber+1}</div></div>
+         
+                 </li>
+               ))}
+            </ul>
          </div>
-         <div className=texte_technologie>
-            <div className=titre_technologie>
-            @lang("$technologie->title")
+         <div className="texte_technologie">
+            <div className="titre_technologie">
+               {selectedTechnology.title}
             </div>
-            <div className=corps_technologie>
-            @lang("$technologie->detail")</div>
+            <div className="corps_technologie">
+               {selectedTechnology.detail}</div>
          </div>
-         <div className=image_technologie>
-            <img src="data:image/png;base64,{{ $technologie->image_path }}" alt="{{ $technologie->title}}">
+         <div className="image_technologie">
+            <img src={`data:image/png;base64,${dataImage}`} alt={selectedTechnology.title}/>
          </div>
       </div>
    </div>
-@stop
     </div>
   )
 }
